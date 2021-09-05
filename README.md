e35a4e8a2b876f3d310c80341e7064d8e43c0626
prashanth <vengalaprashanth24@gmail.com>
2021-09-06 02:59:29 +0530

    fixed some changes

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Audio Player
+# Audio Player Visualizer
 
 The goal of this project is to create a audio player webpage using HTML Canvas in conjunction with JavaScript.
--- script.js
+++ script.js
@@ -5,15 +5,25 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-//Generating height of the audio waves randomly
+//pushing random numbers  to array to generate height of the audio waves randomly
 
-const wavelength = [];
+const lines = [];
+//looping the array
+for (let i = 175; i >= 0; i--) {
+  let length =
+    Math.random() > 0.3
+      ? -(Math.random() * (i - 15) + 90)
+      : Math.random() * (i - 40) + 20;
+  lines.push(length);
+}
 
-for (let i = 165; i >= 0; i--) {
-  let randomHeight =
-    Math.random() > 0.2
-      ? -(Math.random() * (i - 20) + 80)
-      : Math.random() * (i - 10) + 50;
+let lienWidth = 4;
+let init = 10;
+let newInit = init;
 
-  wavelength.push(randomHeight);
+// audio waves using lines
+for (let i = 0; i < lines.length; i++) {
+  ctx.fillStyle = "#e8e8e8";
+  ctx.fillRect(init, 175, lienWidth, lines[i]);
+  init = init + lienWidth + 4;
 }

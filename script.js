5f92b6e3e04fa7545f9e98ab60be8cc4a4b4e4a0
prashanth <vengalaprashanth24@gmail.com>
2021-09-06 02:07:57 +0530

    generated random height of the audio waves

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // script
+
+//Initiating canvas
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+//Generating height of the audio waves randomly
+
+const wavelength = [];
+
+for (let i = 165; i >= 0; i--) {
+  let randomHeight =
+    Math.random() > 0.2
+      ? -(Math.random() * (i - 20) + 80)
+      : Math.random() * (i - 10) + 50;
+
+  wavelength.push(randomHeight);
+}
